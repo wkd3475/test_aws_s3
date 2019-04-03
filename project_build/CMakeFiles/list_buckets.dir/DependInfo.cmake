d476f9acba0366440bb7aaed714207933b8b6e5e
wkd3475 <wkd3475@gmail.com>
2019-04-03 17:49:46 +0900

    fail

---
+++ project_build/CMakeFiles/list_buckets.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wuiing/workspace/aws_test/test_project/list_buckets.cpp" "/home/wuiing/workspace/aws_test/project_build/CMakeFiles/list_buckets.dir/list_buckets.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "PLATFORM_LINUX"
+  "USE_IMPORT_EXPORT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project_build/CMakeFiles/list_buckets.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/list_buckets.dir/list_buckets.cpp.o"
+  "list_buckets.pdb"
+  "list_buckets"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/list_buckets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
